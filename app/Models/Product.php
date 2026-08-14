51c8699bb204020a7e134aa7889a07dd4a09baaa
Pavel <mig20047@gmail.com>
2026-08-14 10:42:56 +0200

    feat: add categories table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+      'name',
+      'slug',
+      'description',
+      'price',
+      'is_active',
+      'stock',
+      'category_id',
+      'image',
+    ];
+
+    protected $casts = [
+      'is_active' => 'boolean',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
