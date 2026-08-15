192cdcf43b2c0b5b5fb3249fb0d4676cb4ab15fe
Pavel <mig20047@gmail.com>
2026-08-15 14:10:31 +0200

    feat: add order resource and order item resource

---
+++ app/Http/Resources/OrderItemResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderItemResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'product_name' => $this->product_name,
+            'quantity' => $this->quantity,
+            'price_at_purchase' => $this->price_at_purchase,
+            'subtotal' => $this->quantity * $this->price_at_purchase,
+        ];
+    }
+}
