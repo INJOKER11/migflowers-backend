641e3e458c5347d75fc82f2b84f7608e82c6b425
Pavel <mig20047@gmail.com>
2026-08-27 13:42:16 +0200

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://migflowers.com',
+        'https://www.migflowers.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
